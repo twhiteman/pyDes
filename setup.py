13df9c95bf2f441a1655f1d6d75b91f972368273
twhiteman <twhiteman>
2009-03-17 03:44:17 +0000

    update to Python3 - additional files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="pyDes",
-      version="1.3.1",
+      version="2.0.0",
       description="Pure python implementation of DES and TRIPLE DES encryption algorithm",
       author="Todd Whiteman",
       author_email="twhitema@gmail.com",
