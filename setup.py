003f067adfd5eaebbfb47966cbfb53397a29bb7e
Todd Whiteman <twhitema@gmail.com>
2016-05-06 17:52:42 -0700

    packaging: updates for python packaging (pypi)

--- setup.py
+++ setup.py
@@ -7,8 +7,17 @@ setup(name="pyDes",
       description="Pure python implementation of DES and TRIPLE DES encryption algorithm",
       author="Todd Whiteman",
       author_email="twhitema@gmail.com",
+      license='MIT',
       url="http://twhiteman.netfirms.com/des.html",
+      classifiers=[
+        'Development Status :: 6 - Mature'
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Security :: Cryptography',
+      ],
       platforms=["All"],
       keywords=["DES", "TRIPLE-DES", "ENCRYPTION", "ALGORITHM", "SECURITY"],
       py_modules=["pyDes"]
-      )
+)
